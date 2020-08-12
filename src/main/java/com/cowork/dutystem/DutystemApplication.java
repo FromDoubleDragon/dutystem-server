bc65755a3f1a062bee029b88690d7bf41f6927cd
WooSungHwan <doqndnffo@gmail.com>
2020-08-12 10:53:56 +0900

    프로젝트 업로드
    빈프로젝트 업로드

---
+++ src/main/java/com/cowork/dutystem/DutystemApplication.java
@@ -0,0 +1,13 @@
+package com.cowork.dutystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DutystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cowork/dutystem/DutystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowork.dutystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
