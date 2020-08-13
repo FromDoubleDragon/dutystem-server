7cfcd26082667e10f82629a6eaf4e2c5513ecaf5
WooSungHwan <doqndnffo@gmail.com>
2020-08-13 15:49:09 +0900

    rest api spring security java config
    java config #1

--- src/main/java/com/cowork/dutystem/configuration/SecurityConfig.java
+++ src/main/java/com/cowork/dutystem/configuration/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -27,12 +29,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .authorizeRequests()    // 이제부터 request의 인증을 확인하겠다.
                     .antMatchers(HttpMethod.OPTIONS, "/**").permitAll() //브라우저의 preflight 요청은 무조건 허용 -> cors 이슈와 관련이 있다.
-                .antMatchers("/test/permit-all").permitAll()     // /test/permit-all 이라는 uri는 무조건 인증허용이다.
                 // 허용할 uri를 하나씩 추가하면 된다.
-                .antMatchers("/test/auth").authenticated()   // /test/auth 라는 uri는 무조건 인증을 받아야한다.
+/* ==================================================== 허용할 URL ============================================================================ */
+                .antMatchers("/api/v1/test/permit-all").permitAll()     // /test/permit-all 이라는 uri는 무조건 인증허용이다.
 
+/* ========================================================================================================================================== */
+
+/* ==================================================== 인증할 URL ============================================================================ */
+                .antMatchers("/api/v1/test/auth").authenticated()   // /test/auth 라는 uri는 무조건 인증을 받아야한다.
+
+/* ========================================================================================================================================== */
                 .antMatchers("/**").authenticated()          // 위를 포함하지 않는 나머지는 모두 인증을 받아야 한다.
+                .anyRequest().permitAll().and()
 
+                .formLogin().disable()
         ;
     }
 }
