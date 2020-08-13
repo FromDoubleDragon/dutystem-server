a81b0538a8b1572a5eac702221f1cc7cf73bdad8
WooSungHwan <doqndnffo@gmail.com>
2020-08-13 16:50:25 +0900

    mybatis 연동 작업중

--- src/main/java/com/cowork/dutystem/service/TestService.java
+++ src/main/java/com/cowork/dutystem/service/TestService.java
@@ -1,9 +1,14 @@
 package com.cowork.dutystem.service;
 
+import com.cowork.dutystem.mapper.TestMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
+
+    @Autowired
+    private TestMapper testMapper;
 
     public Object getTest() throws Exception {
         return "test";
